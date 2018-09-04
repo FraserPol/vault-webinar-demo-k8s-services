865360c4abc3b256095663dd3188595af92e2bb6
Fraser Pollock <fraserpol@gmail.com>
2018-09-03 21:43:58 -0400

    vars

--- variables.tf
+++ variables.tf
@@ -1,45 +1,77 @@
-variable "k8s_instances" {
+variable "k8s_endpoint" {
+  description = "k8s_endpoint"
+}
+
+variable "k8s_master_auth_client_certificate" {
+  description = "k8s_master_auth_client_certificate"
+}
+
+variable "k8s_master_auth_client_key" {
+  description = "k8s_master_auth_client_key"
+}
+
+variable "k8s_master_auth_cluster_ca_certificate" {
+  description = "k8s_master_auth_cluster_ca_certificate"
+}
+
+variable "instance_count" {
   default = 0
- }
+}
 
 variable "vault_host" {
-  default = "localhost"
+  description = "vault_server"
+  default     = "localhost"
 }
 
 variable "vault_port" {
-  default = "8200"
+  description = "vault_port"
+  default     = "8200"
 }
 
 variable "vault_scheme" {
-  default = "http"
+  description = "scheme"
+  default     = "http"
 }
 
 variable "vault_role" {
-  default = "order"
+  description = "vault_role"
+  default     = "order"
+}
+
+variable "postgres_host" {
+  description = "postgres_server"
+  default     = "localhost"
+}
+
+variable "postgres_port" {
+  description = "postgres_port"
+  default     = "5432"
+}
+
+variable "postgres_instance" {
+  description = "postgres_instance"
+  default     = "postgres"
+}
+
+variable "postgres_role" {
+  description = "postgres_role"
+  default     = "database/creds/order"
+}
+
+variable "spring_docker_container" {
+  description = "spring_docker_container"
+  default     = "lanceplarsen/spring-vault-demo"
+}
+
+variable "go_docker_container" {
+  description = "go_docker_container"
+  default     = "lanceplarsen/go-vault-demo"
 }
 
 variable "vault_mount" {
   default = "kubernetes"
 }
 
-variable "postgres_host" {
-  default = "localhost"
-}
-
-variable "postgres_port" {
-  default = "5432"
-}
-
 variable "postgres_mount" {
   default = "database"
 }
-
-variable "postgres_instance" {
-  default = "postgres"
-}
-
-variable "postgres_role" {
-  default = "order"
-}
-
-variable "go_docker_container" {}
