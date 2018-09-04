0ad631d5704526104d93ea136b3914a6189565c2
Fraser Pollock <fraserpol@gmail.com>
2018-09-03 21:41:09 -0400

    missing vars

--- variables.tf
+++ variables.tf
@@ -1,69 +1,45 @@
-variable "k8s_endpoint" {
-  description = "k8s_endpoint"
-}
-
-variable "k8s_master_auth_client_certificate" {
-  description = "k8s_master_auth_client_certificate"
-}
-
-variable "k8s_master_auth_client_key" {
-  description = "k8s_master_auth_client_key"
-}
-
-variable "k8s_master_auth_cluster_ca_certificate" {
-  description = "k8s_master_auth_cluster_ca_certificate"
-}
-
-variable "instance_count" {
+variable "k8s_instances" {
   default = 0
-}
+ }
 
 variable "vault_host" {
-  description = "vault_server"
-  default     = "localhost"
+  default = "localhost"
 }
 
 variable "vault_port" {
-  description = "vault_port"
-  default     = "8200"
+  default = "8200"
 }
 
 variable "vault_scheme" {
-  description = "scheme"
-  default     = "http"
+  default = "http"
 }
 
 variable "vault_role" {
-  description = "vault_role"
-  default     = "order"
+  default = "order"
+}
+
+variable "vault_mount" {
+  default = "kubernetes"
 }
 
 variable "postgres_host" {
-  description = "postgres_server"
-  default     = "localhost"
+  default = "localhost"
 }
 
 variable "postgres_port" {
-  description = "postgres_port"
-  default     = "5432"
+  default = "5432"
+}
+
+variable "postgres_mount" {
+  default = "database"
 }
 
 variable "postgres_instance" {
-  description = "postgres_instance"
-  default     = "postgres"
+  default = "postgres"
 }
 
 variable "postgres_role" {
-  description = "postgres_role"
-  default     = "database/creds/order"
+  default = "order"
 }
 
-variable "spring_docker_container" {
-  description = "spring_docker_container"
-  default     = "lanceplarsen/spring-vault-demo"
-}
-
-variable "go_docker_container" {
-  description = "go_docker_container"
-  default     = "lanceplarsen/go-vault-demo"
-}
+variable "go_docker_container" {}
